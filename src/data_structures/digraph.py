cc832252b2a048dcd6d870be62f15ef17a38bb09
Robert Rodger <woodenrabbit@gmail.com>
2021-04-10 19:46:00 +0200

    convert graph adjacency lists to sets

--- src/data_structures/digraph.py
+++ src/data_structures/digraph.py
@@ -6,18 +6,18 @@ Vertex = int
 class Digraph:
     def __init__(self, num_vertices: int):
         self.num_vertices: int = num_vertices
-        self.adjacency_lists: defaultdict[Vertex, list(Vertex)] = defaultdict(list)
+        self.adjacency_sets: defaultdict[Vertex, set(Vertex)] = defaultdict(set)
 
     def add_edge(self, v: Vertex, w: Vertex):
         # Allows for multiple parallel edges between two vertices
-        self.adjacency_lists[v].append(w)
+        self.adjacency_sets[v].add(w)
 
     def adjacent(self, v: Vertex):
-        return self.adjacency_lists[v]
+        return self.adjacency_sets[v]
 
     def reversed(self):
         reversed = Digraph(self.num_vertices)
-        for source, destinations in self.adjacency_lists.items():
+        for source, destinations in self.adjacency_sets.items():
             for destination in destinations:
                 reversed.add_edge(destination, source)
         return reversed
--- src/data_structures/graph.py
+++ src/data_structures/graph.py
@@ -5,12 +5,12 @@ Vertex = int
 class Graph:
     def __init__(self, num_vertices: int):
         self.num_vertices: int = num_vertices
-        self.adjacency_lists: defaultdict[Vertex, list(Vertex)] = defaultdict(list)
+        self.adjacency_sets: defaultdict[Vertex, set(Vertex)] = defaultdict(set)
 
     def add_edge(self, v: Vertex, w: Vertex):
         # Allows for multiple parallel edges between two vertices
-        self.adjacency_lists[v].append(w)
-        self.adjacency_lists[w].append(v)
+        self.adjacency_sets[v].add(w)
+        self.adjacency_sets[w].add(v)
 
     def adjacent(self, v: Vertex):
-        return self.adjacency_lists[v]
+        return self.adjacency_sets[v]
